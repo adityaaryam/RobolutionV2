72a2e54f25654dba420751a98074e36844b720d9
sarthak <sarthakmdas@gmail.com>
2020-04-10 22:38:03 +0530

    mod3

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use("/public", express.static(__dirname + '/public'));
 members= [];
 
+app.get("/",function(req,res){
+  res.redirect("/home");
+})
 
 app.get("/home",function(req,res){
     res.render("home");
